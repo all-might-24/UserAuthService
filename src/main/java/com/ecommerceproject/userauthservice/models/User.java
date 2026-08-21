a782e8291f1a0654b2ce058defdb04aae5dca894
Bharat Kumar <bharatkumar136@gmail.com>
2026-08-21 20:43:18 +0530

    minor changes

--- src/main/java/com/ecommerceproject/userauthservice/models/User.java
+++ src/main/java/com/ecommerceproject/userauthservice/models/User.java
@@ -1,7 +1,5 @@
 package com.ecommerceproject.userauthservice.models;
 
-import com.ecommerceproject.userauthservice.dtos.UserDto;
-import com.ecommerceproject.userauthservice.models.enums.State;
 import jakarta.persistence.Entity;
 import jakarta.persistence.ManyToMany;
 import lombok.Getter;
@@ -23,13 +21,4 @@ public class User extends BaseEntity {
     @ManyToMany
     private List<Role> roles; // user *---* roles
 
-    public UserDto convertToDTO() {
-        UserDto userDto = new UserDto();
-        userDto.setId(this.getId());
-        userDto.setEmail(this.email);
-        userDto.setUsername(this.username);
-        userDto.setRoles(this.roles);
-
-        return userDto;
-    }
 }
